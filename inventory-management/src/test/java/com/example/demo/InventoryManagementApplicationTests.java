12ca53368986d13e042f592253f2ff98383118c6
ramswaroop11 <ramswaroop1117@gmail.com>
2026-06-13 14:26:18 +0530

    fix: use TestPropertySource to inject H2 config directly for reliable test DB configuration

--- inventory-management/src/test/java/com/example/demo/InventoryManagementApplicationTests.java
+++ inventory-management/src/test/java/com/example/demo/InventoryManagementApplicationTests.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.show-sql=false",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+    "spring.h2.console.enabled=true"
+})
 class InventoryManagementApplicationTests {
 
 	@Test
